fc526c0d2965451e3fb32adfd0914aaa3c40822b
igor_teles <igortelesdesign@gmail.com>
2026-08-25 16:46:12 -0300

    feat: atualiza mockup real do Duna Move

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ import { ProjectItem } from '../types';
 import neneImg from '../assets/images/nene-real-mockup.png';
 import dunaStoreImg from '../assets/images/duna_store_mockup_1787671246927.jpg';
 import portalPacienteImg from '../assets/images/portal_paciente_mockup_1787671277960.jpg';
-import dunaMoveImg from '../assets/images/duna_move_mockup_1787671291818.jpg';
+import dunaMoveImg from '../assets/images/duna-move-real-mockup.png';
 
 export const projectsData: ProjectItem[] = [
   {
